be4b576869a4bf1ad503fd975aa2038241f210fe
Jnanesh <99884442+Jnanesh124@users.noreply.github.com>
2023-10-21 23:09:07 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -30,19 +30,19 @@ Send me any file I will save it in my Database. Also works for channel. Add me t
 
 📡 **Hosted on:** [Heroku](https://heroku.com)
 
-🧑🏻‍💻 **Developer:** [Jnanesh](https://t.me/Beastonejnanesh)
+🧑🏻‍💻 **Developer:** [Jnanesh](https://t.me/jnaneshtn)
 
 
-📢 **Updates Channel:** [ಸಿನಿಮಾ ಹಾಲ್](https://t.me/jnaneshgowdatn)
+📢 **Updates Channel:** [ಸಿನಿಮಾ ಹಾಲ್](https://t.me/jnaneshtn)
 """
 	ABOUT_DEV_TEXT = f"""
-🧑🏻‍💻 **Developer:** @Beastonejnanesh 
+🧑🏻‍💻 **Developer:** @jnaneshtn 
 
 Developer is Super Noob. Just Learning from Official Docs. Please Donate the developer for Keeping the Service Alive.
 
 Also remember that developer will Delete Adult Contents from Database. So better don't Store Those Kind of Things.
 
-[Donate Now](https://t.me/Beastonejnanesh) (love)
+[Donate Now](https://t.me/jnaneshtn) (love)
 """
 	HOME_TEXT = """
 Hi, [{}](tg://user?id={})\n\nThis is Permanent **File Store Bot**.
