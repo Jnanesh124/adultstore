a2528c1f94ac0e832e550337e32e00c6e37bd7df
Jnanesh <99884442+Jnanesh124@users.noreply.github.com>
2023-11-01 19:03:21 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -15,7 +15,7 @@ class Config(object):
 	LOG_CHANNEL = os.environ.get("LOG_CHANNEL", None)
 	BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "1234567890").split())
 	FORWARD_AS_COPY = bool(os.environ.get("FORWARD_AS_COPY", True))
-	BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", False))
+	BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", True))
 	BANNED_CHAT_IDS = list(set(int(x) for x in os.environ.get("BANNED_CHAT_IDS", "-1001362659779 -1001255795497").split()))
 	OTHER_USERS_CAN_SAVE_FILE = bool(os.environ.get("OTHER_USERS_CAN_SAVE_FILE", True))
 	ABOUT_BOT_TEXT = f"""
@@ -36,7 +36,7 @@ Send me any file I will save it in my Database. Also works for channel. Add me t
 📢 **Updates Channel:** [ಸಿನಿಮಾ ಹಾಲ್](https://t.me/jnaneshtn)
 """
 	ABOUT_DEV_TEXT = f"""
-🧑🏻‍💻 **Developer:** @jnaneshtn 
+🧑🏻‍💻 **Developer:** @alonekingjnanesh 
 
 Developer is Super Noob. Just Learning from Official Docs. Please Donate the developer for Keeping the Service Alive.
 
