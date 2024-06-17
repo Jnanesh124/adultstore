29d7ea389e0586a9e347b4d35742c0fdc56b79b5
Jnanesh <99884442+Jnanesh124@users.noreply.github.com>
2024-06-17 21:39:12 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -45,39 +45,7 @@ Also remember that developer will Delete Adult Contents from Database. So better
 [Donate Now](https://t.me/jnaneshtn) (love)
 """
 	HOME_TEXT = """
-Hi, [{}](tg://user?id={})\n\nThis is Permanent **File Store Bot**.
+<b>Hi, [{}](tg://user?id={})\n\nThis is Permanent **File Store Bot**.
 
-𝐊𝐚𝐧𝐧𝐚𝐝𝐚 𝐎𝐧𝐥𝐢𝐧𝐞 𝐌𝐨𝐯𝐢𝐞𝐬  🕶
-
-t.me/+66Rsf3q0p3c2M2Nl
-
-𝐊𝐚𝐧𝐧𝐚𝐝𝐚 𝐇𝐃 𝐌𝐨𝐯𝐢𝐞𝐬  🍿
-
-t.me/+1z2A8ZxGkEM1N2Q1
-
-𝐓𝐫𝐞𝐧𝐝𝐢𝐧𝐠 𝐌𝐨𝐯𝐢𝐞𝐬 𝐇𝐃 📸
-
-t.me/+8K8gz7k6Ry05YzZl
-
-Online Streaming group 💜
-
-t.me/+oQT1f1iF4fU4ZGVl
-
-18+ banned Movies 🔞
-
-t.me/+3xxIFZXTj9E1M2E1
-
-𝐑𝐨𝐜𝐤𝐞𝐫𝐬 𝐊𝐚𝐧𝐧𝐚𝐝𝐚 𝐓𝐯 𝐒𝐞𝐫𝐢𝐚𝐥𝐬 📺
-
-t.me/Rockers_TV_Serials
-
-Sex porn nudi adult video 🔞🔥
-
-t.me/+Ce98xoyvoLcwYThl
-
-𝐉𝐃 𝐈𝐏𝐋 𝐌𝐚𝐭𝐜𝐡 𝐑𝐞𝐩𝐨𝐫𝐭 ⁱᵖˡ ᵐᵃᵗᶜʰ📢
-
-t.me/+rNwi4b0VLU1iODU1
-
-©️@ROCKERSBACKUP 
+made by ©️@ROCKERSBACKUP 🍿</b>
 """
