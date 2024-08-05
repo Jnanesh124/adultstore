33e656734064d12a08a2792c70f5761adbd3bb70
Jnanesh <99884442+Jnanesh124@users.noreply.github.com>
2024-08-05 12:31:55 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -21,7 +21,6 @@ class Config(object):
 	API = environ.get("API", "") # shortlink api
         URL = environ.get("URL", "") # shortlink domain without https://
         VERIFY_TUTORIAL = environ.get("VERIFY_TUTORIAL", "") # how to open link 
-        BOT_USERNAME = environ.get("BOT_USERNAME", "") # bot username without @
         VERIFY = environ.get("VERIFY", "True") # set True Or False and make sure spelling is correct and first letter capital.
 	ABOUT_BOT_TEXT = f"""
 This is Permanent Files Store Bot!
